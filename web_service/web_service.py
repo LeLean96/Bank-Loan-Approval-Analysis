1ff05571a3f2a1297d7f6c5b68d5568c7c3df467
Lelean Shaddod <lelean.shaddod@lit-co.net>
2022-10-07 15:01:04 +0300

    Add flask web service

---
+++ web_service/web_service.py
@@ -0,0 +1,85 @@
+from flask import Flask, jsonify, request
+from flasgger import Swagger
+import pickle
+
+app = Flask(__name__)
+
+swagger = Swagger(app)
+
+@app.route('/predict_loan_status/', methods=['POST'])
+def predict_loan_status():
+    """Endpoint to predict Loan_Status for the given parameters
+    ---
+    parameters:
+      - name: user_data
+        in: body
+        schema:
+          $ref: '#/definitions/UserData'
+        required: true
+    definitions:
+      LoanStatusResult:
+        type: object
+        properties:
+          loan_status:
+            type: boolean
+      UserData:
+        type: object
+        properties:
+          gender:
+            type: string
+            enum: ['Male', 'Female']
+          married:
+            type: string
+            enum: ['No', 'Yes']
+          dependents:
+            type: integer
+          education:
+            type: string
+            enum: ['Not Graduate', 'Graduate']
+          self_employed:
+            type: string
+            enum: ['No', 'Yes']
+          applicant_income:
+            type: number
+          coapplicant_income:
+            type: number
+          loan_amount:
+            type: number
+          loan_amount_term:
+            type: number
+          credit_history:
+            type: number
+          model:
+            type: string
+            enum: ['bayes', 'decision_trees', 'knn', 'logistic_regression', 'svm']
+    responses:
+      200:
+        description: The predicted loan status
+        schema:
+          $ref: '#/definitions/LoanStatusResult'
+        examples:
+          loan_status: true
+    """
+    # predict_loan_status
+    # [0,1,2,1,0,4006,1526,168,360,1]
+    label_mapping = {
+        'Male': 0,
+        'Female': 1,
+        'No': 0,
+        'Yes': 1,
+        'Not Graduate': 0,
+        'Graduate': 1,
+    }
+    data = {key: label_mapping.get(val, val) for key, val in request.json.items()}
+    result = predict_result([data['gender'], data['married'], data['dependents'], data['education'], data['self_employed'], data['applicant_income'], data['coapplicant_income'], data['loan_amount'], data['loan_amount_term'], data['credit_history']], data['model'])
+    return jsonify(dict(loan_status=result))
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+def predict_result(inputvector, modelname):
+    with open(f"../models/{modelname}", 'rb') as f:
+        model = pickle.load(f)
+    return bool(model.predict([inputvector])[0])
